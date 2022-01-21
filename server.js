8e459488863a194ab15c8e743fd7c060ef04513c
Vineel Sai <mail@vineelsai.com>
2022-01-21 19:56:42 +0530

    update some views

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.get('/', (req, res) => {
 
 app.post('/', async (req, res) => {
     let postedUrl = req.body.url
-    postedUrl = postedUrl.toLowerCase()
     postedUrl = postedUrl.trim()
+    postedUrl = postedUrl.toLowerCase()
     postedUrl = postedUrl.replace('https://', '')
     postedUrl = postedUrl.replace('http://', '')
 
@@ -40,7 +40,7 @@ app.post('/', async (req, res) => {
         await axios.get(dbRequestUrl)
             .then(res => res.data)
             .then((json) => {
-                let url = req.protocol + '://' + req.get('host') + '/' + generateUrl(json, postedUrl, req)
+                let url = req.protocol + '://' + req.get('host') + '/' + generateUrl(json, postedUrl)
                 res.render('index', { 'postedUrl': postedUrl, 'error': '', 'url': url })
             })
     } else {
@@ -62,13 +62,12 @@ app.get('/:url', (req, res) => {
         })
 })
 
-
 function isUrl(str) {
-    let regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/
+    let regexp = /^(?:(?:https?):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/
     return regexp.test(str)
 }
 
-function generateUrl(json, postedUrl, req) {
+function generateUrl(json, postedUrl) {
     let urls = []
     let newUrls = []
     let newURL = ''
